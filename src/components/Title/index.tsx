ee28fdddc3715a3b1a909a32e5932ac8942d4dbb
Rafael Caixeta <caixetadev@gmail.com>
2022-07-20 10:56:39 -0300

    feat: add component Title

---
+++ src/components/Title/index.tsx
@@ -0,0 +1,5 @@
+import * as S from "./styles";
+
+export function Title({ children }: { children: string }) {
+  return <S.Title>{children}</S.Title>;
+}
---
+++ src/components/Title/styles.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Title = styled.h2`
+  font-size: 3.6rem;
+  font-weight: 600;
+`;
