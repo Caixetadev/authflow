e93c8484fd1a1be22b667aee97c8c7efa3fc1e9e
Rafael Caixeta <caixetadev@gmail.com>
2022-07-21 03:38:51 -0300

    feat: add position in Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -2,8 +2,12 @@ import styled, { css } from "styled-components";
 
 export const Header = styled.header`
   ${({ theme }) => css`
+    position: sticky;
+    top: 0;
+    left: 0;
     height: 6.5rem;
     width: 100%;
     border-bottom: 1px solid ${theme.colors.gray_border};
+    margin-bottom: 17.5rem;
   `}
 `;
