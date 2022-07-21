f5022e5308e59e5a02ede188fdbcb64967d7b38d
Rafael Caixeta <caixetadev@gmail.com>
2022-07-21 03:40:59 -0300

    feat: add display flex to center the Title

--- src/components/Title/styles.ts
+++ src/components/Title/styles.ts
@@ -1,6 +1,9 @@
 import styled from "styled-components";
 
 export const Title = styled.h2`
+  display: flex;
+  justify-content: center;
   font-size: 3.6rem;
   font-weight: 600;
+  margin-bottom: 2.6rem;
 `;
