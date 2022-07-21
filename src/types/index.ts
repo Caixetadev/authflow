fe34ccff1be8236bd0243af597d2744b656c4025
Rafael Caixeta <caixetadev@gmail.com>
2022-07-21 20:41:37 -0300

    feat: add interface to state

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,11 @@
+export interface IState {
+  isSuccess: boolean;
+  isLoading: boolean;
+  isError: boolean;
+  message: string;
+  user: any;
+}
+
 export interface IUser {
   username: string;
   password: string;
