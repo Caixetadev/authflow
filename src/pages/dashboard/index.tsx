3c945fd606de0528f7db5dec2357f1f4a7014c48
Rafael Caixeta <caixetadev@gmail.com>
2022-07-22 00:32:34 -0300

    feat: add page Dashboard

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+
+import { signOut, useSession } from "next-auth/react";
+import { IState } from "../../types";
+
+function Dashboard() {
+  const { data } = useSession();
+
+  const { user } = useSelector((state: IState) => state);
+
+  return (
+    <>
+      <img
+        src={(data?.user?.image as string) || user.user.photo}
+        alt="Image of Profile"
+      />
+      <button onClick={() => signOut()}>Logout</button>
+    </>
+  );
+}
+
+export default Dashboard;
