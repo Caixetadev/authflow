24e6d8c07da5b884ce532a24f35da29f0408a030
Rafael Caixeta <caixetadev@gmail.com>
2022-07-21 21:11:38 -0300

    feat: adding login function with github

--- src/components/BoxLogin/index.tsx
+++ src/components/BoxLogin/index.tsx
@@ -7,8 +7,8 @@ import * as S from "./styles";
 export function BoxLogin() {
   return (
     <S.Wrapper>
-      {options.map(({ image, alt }) => (
-        <S.Box key={alt}>
+      {options.map(({ image, alt, signIn }) => (
+        <S.Box key={alt} onClick={() => signIn()}>
           <Image src={image} alt={alt} />
         </S.Box>
       ))}
--- src/components/BoxLogin/options.ts
+++ src/components/BoxLogin/options.ts
@@ -2,17 +2,22 @@ import LogoGithub from "../../assets/logoGithub.svg";
 import LogoApple from "../../assets/logoApple.svg";
 import LogoGoogle from "../../assets/logoGoogle.svg";
 
+import { signIn } from "next-auth/react";
+
 export const options = [
   {
     image: LogoGoogle,
     alt: "Logo google",
+    signIn: () => null,
   },
   {
     image: LogoApple,
     alt: "Icon apple",
+    signIn: () => null,
   },
   {
     image: LogoGithub,
     alt: "Logo github",
+    signIn,
   },
 ];
