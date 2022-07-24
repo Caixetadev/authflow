05358daf783ad52df6b1cce1bd82a8a610fdc0aa
Rafael Caixeta <caixetadev@gmail.com>
2022-07-24 20:28:54 -0300

    feat: add types of redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch, useSelector } from "react-redux";
 
 import login from "./modules/auth/auth.store";
 
 export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector = () => useSelector((state: RootState) => state);
 
 export const store = configureStore({
   reducer: login,
