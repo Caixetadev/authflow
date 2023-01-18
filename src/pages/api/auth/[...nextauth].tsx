d80afe7b3fd53689c34ae63b96e9bc0219746417
Rafael Caixeta <caixetadev@gmail.com>
2023-01-18 17:41:23 -0300

    feat: add login in back end by next auth

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -3,6 +3,7 @@ import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import LinkedinProvider from "next-auth/providers/linkedin";
+import CredentialsProvider from "next-auth/providers/credentials";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -17,6 +18,8 @@ export default NextAuth({
           name: profile.name || profile.login,
           email: profile.email,
           image: profile.avatar_url,
+          token: profile.token,
+          refreshToken: "",
         };
       },
     }),
@@ -30,7 +33,69 @@ export default NextAuth({
       clientId: process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_ID as string,
       clientSecret: process.env.NEXT_PUBLIC_LINKEDIN_CLIENT_SECRET as string,
     }),
+
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. 'Sign in with...')
+      name: "Credentials",
+      type: "credentials",
+      // The credentials is used to generate a suitable form on the sign in page.
+      // You can specify whatever fields you are expecting to be submitted.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const payload = {
+          username: credentials?.username,
+          password: credentials?.password,
+        };
+
+        const res = await fetch("https://api-anime-cax.herokuapp.com/auth", {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: { "Content-Type": "application/json" },
+        });
+
+        const user = await res.json();
+
+        // If no error and we have user data, return it
+        if (res.ok && user) {
+          return user.user;
+        }
+        // Return null if user data could not be retrieved
+        return null;
+      },
+    }),
   ],
+
+  pages: {
+    signIn: "/",
+  },
+
+  callbacks: {
+    async jwt({ token, user, account }) {
+      if (account && user) {
+        return {
+          ...token,
+          accessToken: user.token,
+          refreshToken: user.refreshToken,
+        };
+      }
+
+      return token;
+    },
+
+    async session({ session, token }) {
+      if (session?.user) {
+        session.user.accessToken = token.accessToken;
+        session.user.refreshToken = token.refreshToken;
+        session.user.accessTokenExpires = token.accessTokenExpires;
+      }
+
+      return session;
+    },
+  },
 
   secret: process.env.NEXT_PUBLIC_JWT,
 });
